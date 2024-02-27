eca68e100ce9af57fe06abaf81d12a40929ccb40
Jacek <limaniec@gmail.com>
2024-02-27 08:27:37 +0100

    add setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <div className="bg-red-300">tdgdtg</div>;
+  return (
+    <div className="">
+      <div className="container mx-auto bg-red-200">rfrdfgrd</div>
+    </div>
+  );
 }
 
 export default App;
